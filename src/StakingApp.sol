6f94082fd1cc54af97f689ab10e33194d2a27250
Agustin <agustin.acosta@eestn1ee.edu.ar>
2025-12-01 23:20:52 -0300

    feat: initialize foundry project with counter smart contract

---
+++ src/StakingApp.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract StakingApp {
+    
+    address public stakingToken;
+
+    constructor(address _stakingToken, address _admin) {
+        stakingToken = _stakingToken;
+        
+    }
+
+
+
+
+    
+}
